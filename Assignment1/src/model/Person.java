3817d39c2d17c5523ff6a35e81970b5b2b051d39
ke981015 <ke.zh@northeastern.edu>
2021-02-08 14:04:53 +0800

    Assignment1

---
+++ Assignment1/src/model/Person.java
@@ -0,0 +1,248 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+/**
+ *
+ * @author Administrator
+ */
+public class Person {
+    private String FirstName;
+    private String LastName;
+    private String Phone;
+    private String Date;
+    private String Age;
+    private String Height;
+    private String Weight;
+    private String SocialNum;
+    private String Street;
+    private String City;
+    private String State;
+    private String Code;
+    private String BankName;
+    private String BankRoutingNum;
+    private String BankAccountNum;
+    private String Balance;
+    private String Type;
+    private String LicenseNum;
+    private String IssuedDate;
+    private String ExpirDate;
+    private String Blood;
+    private String Picture;
+    private String MedicalNum;
+    private String Alergy1;
+    private String Alergy2;
+    private String Alergy3;
+
+    public String getFirstName() {
+        return FirstName;
+    }
+
+    public void setFirstName(String FirstName) {
+        this.FirstName = FirstName;
+    }
+
+    public String getLastName() {
+        return LastName;
+    }
+
+    public void setLastName(String LastName) {
+        this.LastName = LastName;
+    }
+
+    public String getPhone() {
+        return Phone;
+    }
+
+    public void setPhone(String Phone) {
+        this.Phone = Phone;
+    }
+
+    public String getDate() {
+        return Date;
+    }
+
+    public void setDate(String Date) {
+        this.Date = Date;
+    }
+
+    public String getAge() {
+        return Age;
+    }
+
+    public void setAge(String Age) {
+        this.Age = Age;
+    }
+
+    public String getHeight() {
+        return Height;
+    }
+
+    public void setHeight(String Height) {
+        this.Height = Height;
+    }
+
+    public String getWeight() {
+        return Weight;
+    }
+
+    public void setWeight(String Weight) {
+        this.Weight = Weight;
+    }
+
+    public String getSocialNum() {
+        return SocialNum;
+    }
+
+    public void setSocialNum(String SocialNum) {
+        this.SocialNum = SocialNum;
+    }
+
+    public String getStreet() {
+        return Street;
+    }
+
+    public void setStreet(String Street) {
+        this.Street = Street;
+    }
+
+    public String getCity() {
+        return City;
+    }
+
+    public void setCity(String City) {
+        this.City = City;
+    }
+
+    public String getState() {
+        return State;
+    }
+
+    public void setState(String State) {
+        this.State = State;
+    }
+
+    public String getCode() {
+        return Code;
+    }
+
+    public void setCode(String Code) {
+        this.Code = Code;
+    }
+
+    public String getBankName() {
+        return BankName;
+    }
+
+    public void setBankName(String BankName) {
+        this.BankName = BankName;
+    }
+
+    public String getBankRoutingNum() {
+        return BankRoutingNum;
+    }
+
+    public void setBankRoutingNum(String BankRoutingNum) {
+        this.BankRoutingNum = BankRoutingNum;
+    }
+
+    public String getBankAccountNum() {
+        return BankAccountNum;
+    }
+
+    public void setBankAccountNum(String BankAccountNum) {
+        this.BankAccountNum = BankAccountNum;
+    }
+
+    public String getBalance() {
+        return Balance;
+    }
+
+    public void setBalance(String Balance) {
+        this.Balance = Balance;
+    }
+
+    public String getType() {
+        return Type;
+    }
+
+    public void setType(String Type) {
+        this.Type = Type;
+    }
+
+    public String getLicenseNum() {
+        return LicenseNum;
+    }
+
+    public void setLicenseNum(String LicenseNum) {
+        this.LicenseNum = LicenseNum;
+    }
+
+    public String getIssuedDate() {
+        return IssuedDate;
+    }
+
+    public void setIssuedDate(String IssuedDate) {
+        this.IssuedDate = IssuedDate;
+    }
+
+    public String getExpirDate() {
+        return ExpirDate;
+    }
+
+    public void setExpirDate(String ExpirDate) {
+        this.ExpirDate = ExpirDate;
+    }
+
+    public String getBlood() {
+        return Blood;
+    }
+
+    public void setBlood(String Blood) {
+        this.Blood = Blood;
+    }
+
+    public String getPicture() {
+        return Picture;
+    }
+
+    public void setPicture(String Picture) {
+        this.Picture = Picture;
+    }
+
+    public String getMedicalNum() {
+        return MedicalNum;
+    }
+
+    public void setMedicalNum(String MedicalNum) {
+        this.MedicalNum = MedicalNum;
+    }
+
+    public String getAlergy1() {
+        return Alergy1;
+    }
+
+    public void setAlergy1(String Alergy1) {
+        this.Alergy1 = Alergy1;
+    }
+
+    public String getAlergy2() {
+        return Alergy2;
+    }
+
+    public void setAlergy2(String Alergy2) {
+        this.Alergy2 = Alergy2;
+    }
+
+    public String getAlergy3() {
+        return Alergy3;
+    }
+
+    public void setAlergy3(String Alergy3) {
+        this.Alergy3 = Alergy3;
+    }
+    
+}
