05e70be6502827a9a754a1757f2b4997bae68805
ke981015 <ke.zh@northeastern.edu>
2021-02-22 00:10:26 +0800

    Assignment2-Part2

---
+++ Assignment/Assignment2/Assignment2-Part2/src/model/VitalSigns.java
@@ -0,0 +1,87 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+/**
+ *
+ * @author Administrator
+ */
+public class VitalSigns {
+    
+    private String name;
+    private float age;
+    private int rate;
+    private int heart;
+    private int pressure;
+    private float kilos;
+    private float pounds;
+    private String data;
+    
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+    public String getName() {
+        return name;
+    }
+    
+    public void setAge(float age) {
+        this.age = age;
+    }
+    
+    public float getAge() {
+        return age;
+    }
+    
+    public void setRate(int rate) {
+        this.rate = rate;
+    }
+    
+    public int getRate() {
+        return rate;
+    }
+    
+    public void setHeart(int heart) {
+        this.heart = heart;
+    }
+    
+    public int getHeart() {
+        return heart;
+    }
+    
+    public void setPressure(int pressure) {
+        this.pressure = pressure;
+    }
+    
+    public int getPressure() {
+        return pressure;
+    }
+    
+    public void setKilos(float kilos) {
+        this.kilos = kilos;
+    }
+    
+    public float getKilos() {
+        return kilos;
+    }
+    
+    public void setPounds(float pounds) {
+        this.pounds = pounds;
+    }
+    
+    public float getPounds() {
+        return pounds;
+    }
+    
+    public String getData() {
+        return data;
+    }
+    
+    public void setData(String data) {
+        this.data = data;
+    }
+    
+}
